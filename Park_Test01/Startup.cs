acd06f3b9ec272eb715bdb87306e30f9d350ac83
luizvenesi <luizvenesi@luizvenesi.com.br>
2017-11-30 19:50:54 -0500

    Initial commit

---
+++ Park_Test01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Park_Test01.Startup))]
+namespace Park_Test01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Park_Test01/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Park_Test01.MvcApplication" Language="C#" %>
